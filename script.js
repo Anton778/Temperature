13c2b03b6999d6f519e7af4826753b47442e89bd
Anton T <61037544+Anton778@users.noreply.github.com>
2024-03-01 13:22:49 +0700

    Update script.js

--- script.js
+++ script.js
@@ -53,14 +53,5 @@ document.addEventListener('DOMContentLoaded', function() {
         }
       });
     })
-
-    // Получаем данные о последней температуре
-  fetch('https://api.thingspeak.com/channels/2447664/feeds.json?api_key=YGABPVZSCX5NJB3A&results=1')
-    .then(response => response.json())
-    .then(data => {
-      const currentTemperature = parseFloat(data.feeds[0].field1);
-      const temperatureElement = document.getElementById('currentTemperature');
-      temperatureElement.textContent = 'Текущая температура: ' + currentTemperature + '°C';
-    })
     .catch(error => console.error('Ошибка при получении данных:', error));
 });
