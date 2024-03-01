5b791c4df407408e78f85348d0f431f45c037259
Anton T <61037544+Anton778@users.noreply.github.com>
2024-03-01 13:57:15 +0700

    Update currentTemperature.js

--- currentTemperature.js
+++ currentTemperature.js
@@ -7,7 +7,22 @@ function showCurrentTemperature() {
     .then(data => {
       const currentTemperature = parseFloat(data.feeds[0].field1);
       const temperatureElement = document.getElementById('currentTemperature');
-      temperatureElement.textContent = 'Текущая температура: ' + currentTemperature + '°C';
+      temperatureElement.textContent = 'Текущая температура: ' + currentTemperature + ' °C';
+
+      // Находим максимальную температуру
+      const temperatureData = data.feeds.map(feed => parseFloat(feed.field1));
+      const maxTemperature = Math.max(...temperatureData);
+      const maxTemperatureElement = document.createElement('p');
+      maxTemperatureElement.textContent = `Максимальная температура: ${maxTemperature} °C`;
+      maxTemperatureElement.style.color = 'red'; // Красный цвет для максимальной температуры
+      temperatureElement.parentNode.appendChild(maxTemperatureElement);
+
+      // Находим минимальную температуру
+      const minTemperature = Math.min(...temperatureData);
+      const minTemperatureElement = document.createElement('p');
+      minTemperatureElement.textContent = `Минимальная температура: ${minTemperature} °C`;
+      minTemperatureElement.style.color = 'blue'; // Синий цвет для минимальной температуры
+      temperatureElement.parentNode.appendChild(minTemperatureElement);
     })
     .catch(error => console.error('Ошибка при получении данных о текущей температуре:', error));
 }
