c10ece2bad11b5c6a7ab14d528b6ef5f8af31837
Anton T <61037544+Anton778@users.noreply.github.com>
2024-03-01 13:24:09 +0700

    Create currentTemperature.js

---
+++ currentTemperature.js
@@ -0,0 +1,16 @@
+// currentTemperature.js
+
+// Получаем данные о последней температуре и отображаем их на странице
+function showCurrentTemperature() {
+  fetch('https://api.thingspeak.com/channels/2447664/feeds.json?api_key=YGABPVZSCX5NJB3A&results=1')
+    .then(response => response.json())
+    .then(data => {
+      const currentTemperature = parseFloat(data.feeds[0].field1);
+      const temperatureElement = document.getElementById('currentTemperature');
+      temperatureElement.textContent = 'Текущая температура: ' + currentTemperature + '°C';
+    })
+    .catch(error => console.error('Ошибка при получении данных о текущей температуре:', error));
+}
+
+// Вызываем функцию для отображения текущей температуры
+showCurrentTemperature();
