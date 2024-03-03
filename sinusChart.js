46bccd46ca781712408c1a42e850a96273b31648
Anton T <61037544+Anton778@users.noreply.github.com>
2024-03-03 09:26:26 +0700

    Update sinusChart.js

--- sinusChart.js
+++ sinusChart.js
@@ -1,45 +1,108 @@
-// sinusChart.js
 
+// Глобальная переменная для хранения URL по умолчанию
+let defaultUrl = 'https://api.thingspeak.com/channels/2447664/feeds.json?api_key=YGABPVZSCX5NJB3A';
 document.addEventListener('DOMContentLoaded', function() {
-    // Создаем массив значений x от 0 до 2*pi с шагом 0.1
-    const xValues = [];
-    for (let x = 0; x <= Math.PI * 2; x += 0.1) {
-        xValues.push(x);
+    // Функция для загрузки данных и построения графика
+    function fetchDataAndDrawChart(url) {
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                const temperatureData = data.feeds.map(feed => parseFloat(feed.field1));
+                const timeLabels = data.feeds.map(feed => {
+                    const date = new Date(feed.created_at);
+                    date.setHours(date.getHours());
+                    return date.toLocaleString('ru-RU', { hour: 'numeric', minute: 'numeric', day: 'numeric', month: 'numeric' });
+                });
+                const ctx = document.getElementById('temperatureChart').getContext('2d');
+                const chart = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: timeLabels,
+                        datasets: [{
+                            label: 'Температура в городе Бердске',
+                            data: temperatureData,
+                            borderColor: 'black',
+                            backgroundColor: 'lightblue',
+                            fill: false,
+                        }]
+                    },
+                    options: {
+                        responsive: true,
+                        maintainAspectRatio: true,
+                        scales: {
+                            xAxes: [{
+                                type: 'time',
+                                time: {
+                                    unit: 'minute',
+                                    stepSize: 100,
+                                    tooltipFormat: 'HH:mm, DD.MM.YYYY',
+                                    displayFormats: {
+                                        hour: 'HH',
+                                        day: 'DD.MM'
+                                    }
+                                },
+                                ticks: {
+                                    maxRotation: 0,
+                                    autoSkip: false,
+                                    maxTicksLimit: 10
+                                }
+                            }],
+                            yAxes: [{
+                                scaleLabel: {
+                                    display: true,
+                                    labelString: 'Температура (°C)'
+                                }
+                            }]
+                        }
+                    }
+                });
+                const maxTemperature = Math.max(...temperatureData);
+                const minTemperature = Math.min(...temperatureData);
+                const currentTemperature = temperatureData[temperatureData.length - 1];
+                function highlightMinMaxTemperature(currentTemperature, maxTemperature, minTemperature) {
+                    const dataset = chart.data.datasets[0];
+                    const data = dataset.data;
+                    const backgroundColors = Array(data.length).fill('lightblue');
+                    const maxTemperatureIndex = data.findIndex(temp => temp === maxTemperature);
+                    const minTemperatureIndex = data.findIndex(temp => temp === minTemperature);
+                    if (maxTemperatureIndex !== -1) {
+                        backgroundColors[maxTemperatureIndex] = 'red';
+                    }
+                    if (minTemperatureIndex !== -1) {
+                        backgroundColors[minTemperatureIndex] = 'blue';
+                    }
+                    dataset.backgroundColor = backgroundColors;
+                    chart.update();
+                }
+                highlightMinMaxTemperature(currentTemperature, maxTemperature, minTemperature);
+                const minMaxTemperaturesElement = document.getElementById('minMaxTemperatures');
+                minMaxTemperaturesElement.innerHTML = `
+                    <p>Текущая температура: ${currentTemperature}°C</p>
+                    <p>Минимальная температура: ${minTemperature}°C была зафиксирована ${getTimestampOfTemperature(minTemperature, data)}</p>
+                    <p>Максимальная температура: ${maxTemperature}°C была зафиксирована ${getTimestampOfTemperature(maxTemperature, data)}</p>
+                `;
+                function getTimestampOfTemperature(temperature, data) {
+                    const index = data.feeds.findIndex(feed => parseFloat(feed.field1) === temperature);
+                    if (index !== -1) {
+                        const timestamp = new Date(data.feeds[index].created_at);
+                        return timestamp.toLocaleString('ru-RU');
+                    }
+                    return 'неизвестно';
+                }
+            })
+            .catch(error => console.error('Ошибка при получении данных:', error));
     }
-
-    // Создаем массив значений y, где y = sin(x)
-    const yValues = xValues.map(x => Math.sin(x));
-
-    // Получаем контекст canvas
-    const ctx = document.getElementById('sinusChart').getContext('2d');
-
-    // Создаем график синуса
-    const sinusChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: xValues, // Задаем значения x
-            datasets: [{
-                label: 'sin(x)', // Задаем название датасета
-                data: yValues, // Задаем значения y
-                borderColor: 'blue', // Задаем цвет линии
-                backgroundColor: 'transparent', // Прозрачный фон
-            }]
-        },
-        options: {
-            scales: {
-                xAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'x' // Название оси x
-                    }
-                }],
-                yAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        labelString: 'sin(x)' // Название оси y
-                    }
-                }]
-            }
-        }
+    // Обработчик кнопки "За день"
+    document.getElementById('btnDay').addEventListener('click', function() {
+        const url = 'https://api.thingspeak.com/channels/2447664/feeds.json?api_key=YGABPVZSCX5NJB3A&days=1';
+        fetchDataAndDrawChart(url);
     });
+
+    // Обработчик кнопки "За всё время"
+    // Обработчик кнопки "За все время"
+    document.getElementById('btnAllTime').addEventListener('click', function() {
+        fetchDataAndDrawChart(defaultUrl);
+    });
+    // Загружаем график по умолчанию при загрузке страницы
+    fetchDataAndDrawChart(defaultUrl);
 });
