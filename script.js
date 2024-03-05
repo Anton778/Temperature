19d51ff414669ce3172cae3b390e53ac2eb1df97
Anton T <61037544+Anton778@users.noreply.github.com>
2024-03-05 09:54:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // script.js
 // Глобальная переменная для хранения URL по умолчанию
 let defaultUrl = 'https://api.thingspeak.com/channels/2447664/feeds.json?api_key=YGABPVZSCX5NJB3A';
+let chart; // Добавляем переменную для хранения объекта Chart
+
 document.addEventListener('DOMContentLoaded', function() {
-    let chart; // Добавляем переменную для хранения объекта Chart
     // Функция для загрузки данных и построения графика
     function fetchDataAndDrawChart(url) {
         fetch(url)
@@ -15,7 +16,7 @@ document.addEventListener('DOMContentLoaded', function() {
                     return date.toLocaleString('ru-RU', { hour: 'numeric', minute: 'numeric', day: 'numeric', month: 'numeric' });
                 });
                 const ctx = document.getElementById('temperatureChart').getContext('2d');
-                const chart = new Chart(ctx, {
+                chart = new Chart(ctx, {
                     type: 'line',
                     data: {
                         labels: timeLabels,
@@ -57,55 +58,35 @@ document.addEventListener('DOMContentLoaded', function() {
                         }
                     }
                 });
-                const maxTemperature = Math.max(...temperatureData);
-                const minTemperature = Math.min(...temperatureData);
-                const currentTemperature = temperatureData[temperatureData.length - 1];
-                function highlightMinMaxTemperature(currentTemperature, maxTemperature, minTemperature) {
-                    const dataset = chart.data.datasets[0];
-                    const data = dataset.data;
-                    const backgroundColors = Array(data.length).fill('lightblue');
-                    const maxTemperatureIndex = data.findIndex(temp => temp === maxTemperature);
-                    const minTemperatureIndex = data.findIndex(temp => temp === minTemperature);
-                    if (maxTemperatureIndex !== -1) {
-                        backgroundColors[maxTemperatureIndex] = 'red';
-                    }
-                    if (minTemperatureIndex !== -1) {
-                        backgroundColors[minTemperatureIndex] = 'blue';
-                    }
-                    dataset.backgroundColor = backgroundColors;
-                    chart.update();
-                }
-                highlightMinMaxTemperature(currentTemperature, maxTemperature, minTemperature);
-                const minMaxTemperaturesElement = document.getElementById('minMaxTemperatures');
-                minMaxTemperaturesElement.innerHTML = `
-                    <p>Текущая температура: ${currentTemperature}°C</p>
-                    <p>Минимальная температура: ${minTemperature}°C была зафиксирована ${getTimestampOfTemperature(minTemperature, data)}</p>
-                    <p>Максимальная температура: ${maxTemperature}°C была зафиксирована ${getTimestampOfTemperature(maxTemperature, data)}</p>
-                `;
-                function getTimestampOfTemperature(temperature, data) {
-                    const index = data.feeds.findIndex(feed => parseFloat(feed.field1) === temperature);
-                    if (index !== -1) {
-                        const timestamp = new Date(data.feeds[index].created_at);
-                        return timestamp.toLocaleString('ru-RU');
-                    }
-                    return 'неизвестно';
-                }
             })
             .catch(error => console.error('Ошибка при получении данных:', error));
     }
+
     // Обработчик кнопки "За день"
     document.getElementById('btnDay').addEventListener('click', function() {
         const url = 'https://api.thingspeak.com/channels/2447664/feeds.json?api_key=YGABPVZSCX5NJB3A&days=1';
-        chart.destroy();
+        if (chart) {
+            chart.destroy();
+        }
         fetchDataAndDrawChart(url);
     });
 
-    // Обработчик кнопки "За всё время"
     // Обработчик кнопки "За все время"
     document.getElementById('btnAllTime').addEventListener('click', function() {
-        chart.destroy();
+        if (chart) {
+            chart.destroy();
+        }
         fetchDataAndDrawChart(defaultUrl);
     });
+
+    // Обработчик кнопки "Удалить график"
+    document.getElementById('btnRemoveChart').addEventListener('click', function() {
+        if (chart) {
+            chart.destroy();
+            chart = null; // Сбрасываем переменную chart
+        }
+    });
+
     // Загружаем график по умолчанию при загрузке страницы
     fetchDataAndDrawChart(defaultUrl);
 });
