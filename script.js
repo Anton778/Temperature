7fb4e4277d2831e9ce5decd53fe5821b119eec35
Anton T <61037544+Anton778@users.noreply.github.com>
2024-03-08 11:23:43 +0700

    Update script.js

--- script.js
+++ script.js
@@ -52,17 +52,6 @@ document.addEventListener('DOMContentLoaded', function() {
             });
     }
 
-    // Функция поиска температуры для заданного времени
-    function findTemperatureForTime(time, feeds) {
-        // Проходим по всем данным и ищем температуру для заданного времени
-        for (const feed of feeds) {
-            const feedTime = new Date(feed.created_at).toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' });
-            if (feedTime === time) {
-                return parseFloat(feed.field1); // Возвращаем температуру для найденного времени
-            }
-        }
-        return null; // Если данных нет для заданного времени, возвращаем null
-    }
 
     // Функция для отображения графика
     function drawChart(timeLabels, temperatureData) {
